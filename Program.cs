a3f0b41b01280629a4d06153b15913f32a83df1c
Praful Gabhane <45679855+prafulgabhane@users.noreply.github.com>
2023-01-13 17:28:13 +0530

    Added to remote using git

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using FindMaxGenerics;
+
+namespace FindMaxGenerics
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find the Max Num of 3 Variables Program");
+
+            string X = "Apple";
+            string Y = "Peach";
+            string Z = "Banana";
+            
+	    //output
+            string val = FindMax<string>.MaxOfString(X, Y, Z);
+            Console.WriteLine("The greater String is " + val);
+        }
+    }
+}
